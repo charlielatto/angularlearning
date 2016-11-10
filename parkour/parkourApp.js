685e6408bc27b93de99cedfa29bf5b9e07f5316d
Latto <charles.latto@bt.com>
2016-11-10 13:21:21 +0000

    Backlog view changed to stacked, added router and 2nd controller in js for Sprint view

--- parkour/parkourApp.js
+++ parkour/parkourApp.js
@@ -1,56 +1,147 @@
-var app = angular.module('parkour',[]);
+var app = angular.module('parkour',['ui.router']);
 
-app.factory('backlog',[function(){
-	var o ={
-		tasks:[]
-	};
-	return o;
-}]);
-
-app.factory('sprint',[function(){
-	var o ={
-		tasks:[]
-	};
-	return o;
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider,$urlRouterProvider){
+	$stateProvider.state('backlog',{
+			url: '/backlog',
+			templateUrl: '/backlog.html',
+			controller: 'MainCtrl'
+		});
+	$stateProvider.state('sprint',{
+			url: '/sprint',
+			templateUrl: '/sprint.html',
+			controller: 'SprintCtrl'
+		});
+	
+	$urlRouterProvider.otherwise('backlog');
 }]);
 
 app.controller('MainCtrl',[
 '$scope',
 'sprint',
 'backlog',
-function($scope){
-	$scope.title= "Parkour";
+'setUp',
+function($scope,sprint,backlog,setUp){
 	$scope.sprintNum = 1;
-	$scope.backlog = [{title:"Task 1", time: 8, priority: 2},
-		{title:"Task 2", time: 16, priority: 1},
-		{title:"Task 3", time: 4, priority: 3},
-		{title:"Task 4", time: 4, priority: 4}];
 	
-	$scope.sprint = [{title:"Task 5", time: 6, priority: 3},
-		{title:"Task 6", time: 13, priority: 2},
-		{title:"Task 7", time: 8, priority: 4},
-		{title:"Task 8", time: 4, priority: 1}];
+	$scope.backlog = backlog.tasks;
+	$scope.sprint = sprint.tasks;
+	
+	$scope.initBacklogKanban = function(){
+		$( function() {
+			$( "#backlog" ).sortable({
+				connectWith: ".connectedSort",
+				receive: function( event, ui ) { setUp.updateElem(event,ui,$scope.backlog,$scope.sprint); },
+				//stop: function( event, ui ) { alert('Stop!'); }
+			}).disableSelection();
+			
+		} );
 		
-	$scope.update = function(event,ui){
-		var data = ui.item.data().$scope.task;
-		console.log(data);
+		$( function() {
+			$( "#sprint" ).sortable({
+				connectWith: ".connectedSort",
+				receive: function( event, ui ) { setUp.updateElem(event,ui,$scope.sprint,$scope.backlog); },
+				//stop: function( event, ui ) { alert('Stop!'); }
+			}).disableSelection();
+			
+		} );
+	};
+	
+	$scope.initBacklogKanban();
+	
+	$scope.applyPriority = function(priority){
+		return setUp.applyPriority(priority);
 	}
 	
-	$scope.initKanban = function(){
+	$scope.popUp = function(task){
+		setUp.popUp(task);
+	};
+}]);
+
+app.controller('SprintCtrl',[
+'$scope',
+'sprint',
+'setUp',
+function($scope,sprint,setUp){
+	$scope.placeholder = "Sprint";
+	$scope.sprint = sprint.tasks;
+	
+	var updateElem = function(event,ui,board){
+		var id = ui.item.data().$scope.task.id;
+		//console.log(id);
+		//console.log(board);
+	}
+	
+	
+	$scope.initSprintKanban = function(){
 		$( function() {
-			$( "#backlog,#sprint" ).sortable({
+			$( "#sprint" ).sortable({
 				connectWith: ".connectedSort",
 				update: function( event, ui ) {
-					$scope.update(event,ui);
+					updateElem(event,ui);
 				}
 			}).disableSelection();
 			
 		} );
 	};
 	
-	$scope.initKanban();
+	$scope.initSprintKanban();
 	
 	$scope.applyPriority = function(priority){
+		return setUp.applyPriority(priority);
+	}
+	
+	$scope.popUp = function(task){
+		setUp.popUp(task);
+	};
+}]);
+
+
+app.factory('backlog',[function(){
+	var o ={
+		tasks:[{id:1,title:"Task 1", time: 8, priority: 2},
+		{id:2,title:"Task 2", time: 16, priority: 1},
+		{id:3,title:"Task 3", time: 4, priority: 3},
+		{id:4,title:"Task 4", time: 4, priority: 4}]
+	};
+	return o;
+}]);
+
+app.factory('sprint',[function(){
+	var o ={
+		tasks:[{id:5,title:"Task 5", time: 6, priority: 3},
+		{id:6,title:"Task 6", time: 13, priority: 2},
+		{id:7,title:"Task 7", time: 8, priority: 4},
+		{id:8,title:"Task 8", time: 4, priority: 1}]
+	};
+	return o;
+}]);
+
+app.service('setUp',function(){	
+
+	this.updateElem = function(event,ui,board,fromBoard){
+		var id = ui.item.data().$scope.task.id;
+		//console.log(id);
+		//console.log(board);
+		//console.log(fromBoard);
+		var index = -1;
+		for(var i = 0; i < fromBoard.length; i++) {
+			if (fromBoard[i].id === id) {
+				index = i;
+				break;
+			}
+		}
+		if(index === -1) {return;}
+		board.push(fromBoard[index]);
+		fromBoard.splice(index,1);
+		//console.log("done");
+		//console.log(board);
+		//console.log(fromBoard);
+	}
+	
+	this.applyPriority = function(priority){
 		if (priority == 1){
 			return "callout alert";
 		} else if (priority == 2){
@@ -62,7 +153,7 @@ function($scope){
 		}	
 	};
 	
-	$scope.popUp = function(task){
+	this.popUp = function(task){
 		alert(task.title);
 	};
-}]);
+});
